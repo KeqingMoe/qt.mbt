3138e98e6c3326d09ccdd2305b77756087521526
Teru Shigure <me@keqing.moe>
2026-03-16 09:09:59 +0800

    feat: add rest signals of QAbstractButton

--- stub/widgets/QAbstractButton.cpp
+++ stub/widgets/QAbstractButton.cpp
@@ -10,6 +10,33 @@ using Clicked = SignalAdapter< //
     } //
     >;
 
+using Pressed = SignalAdapter< //
+    QAbstractButton,
+    &QAbstractButton::pressed,
+    Unit,
+    []() {
+        return Unit{};
+    } //
+    >;
+
+using Released = SignalAdapter< //
+    QAbstractButton,
+    &QAbstractButton::released,
+    Unit,
+    []() {
+        return Unit{};
+    } //
+    >;
+
+using Toggled = SignalAdapter< //
+    QAbstractButton,
+    &QAbstractButton::toggled,
+    Bool,
+    [](bool x) {
+        return Bool::make(x);
+    } //
+    >;
+
 extern "C"
 {
     auto QAbstractButton_setText(qt<QAbstractButton> self, String text) -> void
@@ -20,6 +47,21 @@ extern "C"
     auto QAbstractButton_clicked(qt<QAbstractButton> self) -> box<Clicked>
     {
         return box<Clicked>::make(*self.repr);
+    }
+
+    auto QAbstractButton_pressed(qt<QAbstractButton> self) -> box<Pressed>
+    {
+        return box<Pressed>::make(*self.repr);
+    }
+
+    auto QAbstractButton_released(qt<QAbstractButton> self) -> box<Released>
+    {
+        return box<Released>::make(*self.repr);
+    }
+
+    auto QAbstractButton_toggled(qt<QAbstractButton> self) -> box<Toggled>
+    {
+        return box<Toggled>::make(*self.repr);
     }
 
     auto QAbstractButton_as_QWidget(qt<QAbstractButton> self) -> qt<QWidget>

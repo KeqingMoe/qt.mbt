e33bd40664f5d3ef5f953374ee892621b1a2ca9a
Teru Shigure <me@keqing.moe>
2026-03-16 13:20:04 +0800

    feat: improve QAbstractButton

--- stub/widgets/QAbstractButton.cpp
+++ stub/widgets/QAbstractButton.cpp
@@ -1,9 +1,99 @@
 #include <QAbstractButton>
 #include <qt.hpp>
 
+QMETHOD(QAbstractButton, animateClick)->void
+{
+    self->animateClick();
+}
+
+QMETHOD(QAbstractButton, autoExclusive)->Bool
+{
+    return Bool::make(self->autoExclusive());
+}
+
+QMETHOD(QAbstractButton, autoRepeat)->Bool
+{
+    return Bool::make(self->autoRepeat());
+}
+
+QMETHOD(QAbstractButton, autoRepeatDelay)->Int
+{
+    return self->autoRepeatDelay();
+}
+
+QMETHOD(QAbstractButton, autoRepeatInterval)->Int
+{
+    return self->autoRepeatInterval();
+}
+
+QMETHOD(QAbstractButton, click)->void
+{
+    self->click();
+}
+
+QMETHOD(QAbstractButton, isCheckable)->Bool
+{
+    return Bool::make(self->isCheckable());
+}
+
+QMETHOD(QAbstractButton, isChecked)->Bool
+{
+    return Bool::make(self->isChecked());
+}
+
+QMETHOD(QAbstractButton, isDown)->Bool
+{
+    return Bool::make(self->isDown());
+}
+
+QMETHOD(QAbstractButton, setAutoExclusive, Bool value)->void
+{
+    self->setAutoExclusive(value);
+}
+
+QMETHOD(QAbstractButton, setAutoRepeat, Bool value)->void
+{
+    self->setAutoRepeat(value);
+}
+
+QMETHOD(QAbstractButton, setAutoRepeatDelay, Int delay)->void
+{
+    self->setAutoRepeatDelay(delay);
+}
+
+QMETHOD(QAbstractButton, setAutoRepeatInterval, Int interval)->void
+{
+    self->setAutoRepeatInterval(interval);
+}
+
+QMETHOD(QAbstractButton, setCheckable, Bool value)->void
+{
+    self->setCheckable(value);
+}
+
+QMETHOD(QAbstractButton, setChecked, Bool value)->void
+{
+    self->setChecked(value);
+}
+
+QMETHOD(QAbstractButton, setDown, Bool value)->void
+{
+    self->setDown(value);
+}
+
 QMETHOD(QAbstractButton, setText, String text)->void
 {
     self->setText(str::mbt_to_qt(text));
+}
+
+QMETHOD(QAbstractButton, text)->String
+{
+    return str::qt_to_mbt(self->text());
+}
+
+QMETHOD(QAbstractButton, toggle)->void
+{
+    self->toggle();
 }
 
 SIGNAL_DEF(QAbstractButton, clicked, Bool, Bool::make)

46fa7db1a9a660eac0c1fb55ec4fbc6d4b559d5b
Teru Shigure <me@keqing.moe>
2026-03-17 22:13:13 +0800

    refactor: move signal slot methods to QObject.cpp

--- stub/core/QObject.cpp
+++ stub/core/QObject.cpp
@@ -1,6 +1,17 @@
 #include <QObject>
 #include <qt.hpp>
 
+QSTATIC(QObject, connect, ErasedSignal signal, ErasedSlot slot)->Connection
+{
+    return signal->repr->connect_erased(slot);
+}
+
+QSTATIC(QObject, disconnect, Connection connection)->Bool
+{
+    auto flag = QObject::disconnect(*connection);
+    return Bool::make(flag);
+}
+
 QMETHOD(QObject, is_null)->bool
 {
     return self.is_null();
--- stub/core/signal.cpp
+++ stub/core/signal.cpp
@@ -1,16 +1,4 @@
-#include <QObject>
 #include <signal.hpp>
-
-auto QObject_connect(ErasedSignal signal, ErasedSlot slot) -> Connection
-{
-    return signal->repr->connect_erased(slot);
-}
-
-auto QObject_disconnect(Connection connection) -> mbt::Bool
-{
-    auto flag = QObject::disconnect(*connection);
-    return mbt::Bool::make(flag);
-}
 
 auto Connection_is_valid(Connection connection) -> mbt::Bool
 {

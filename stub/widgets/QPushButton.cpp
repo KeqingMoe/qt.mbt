7576a566ac9a734996f7807fad4625174ecccb64
Teru Shigure <me@keqing.moe>
2026-03-18 00:00:17 +0800

    refactor: remove QPushButton::on_clicked

--- stub/widgets/QPushButton.cpp
+++ stub/widgets/QPushButton.cpp
@@ -6,11 +6,4 @@ QSTATIC(QPushButton, new)->qt<QPushButton>
     return qt<QPushButton>::make();
 }
 
-QMETHOD(QPushButton, on_clicked, fn<Unit()> cb)->void
-{
-    QObject::connect(self.get(), &QPushButton::clicked, [cb = own{cb}] {
-        cb.repr();
-    });
-}
-
 COVARIANT(QPushButton, QAbstractButton)

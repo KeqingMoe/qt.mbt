710b585d3be6f84a32054b01c73e92f0f15e1df8
Teru Shigure <me@keqing.moe>
2026-03-18 13:13:37 +0800

    feat: add QCheckBox

---
+++ stub/widgets/QCheckBox.cpp
@@ -0,0 +1,33 @@
+#include <QCheckBox>
+#include <qt.hpp>
+
+QSTATIC(QCheckBox, new)->qt<QCheckBox>
+{
+    return qt<QCheckBox>::make();
+}
+
+QMETHOD(QCheckBox, checkState)->Int
+{
+    return self->checkState();
+}
+
+QMETHOD(QCheckBox, isTristate)->Bool
+{
+    return Bool::make(self->isTristate());
+}
+
+QMETHOD(QCheckBox, setCheckState, Int state)->void
+{
+    self->setCheckState(static_cast<Qt::CheckState>(state));
+}
+
+QMETHOD(QCheckBox, setTristate, Bool tristate)->void
+{
+    self->setTristate(tristate);
+}
+
+SIGNAL_DEF(QCheckBox, checkStateChanged, Int, [](auto state) { return state; })
+
+SIGNAL_DEF(QCheckBox, stateChanged, Int, [](auto state) { return state; })
+
+COVARIANT(QCheckBox, QAbstractButton)

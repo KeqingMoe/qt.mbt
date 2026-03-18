61e5cdcc1a3d60664c964d0d972649ad03853bc9
Teru Shigure <me@keqing.moe>
2026-03-18 13:14:24 +0800

    feat: add QGroupBox

---
+++ stub/widgets/QGroupBox.cpp
@@ -0,0 +1,51 @@
+#include <QGroupBox>
+#include <qt.hpp>
+
+QSTATIC(QGroupBox, new)->qt<QGroupBox>
+{
+    return qt<QGroupBox>::make();
+}
+
+QMETHOD(QGroupBox, isCheckable)->Bool
+{
+    return Bool::make(self->isCheckable());
+}
+
+QMETHOD(QGroupBox, isChecked)->Bool
+{
+    return Bool::make(self->isChecked());
+}
+
+QMETHOD(QGroupBox, isFlat)->Bool
+{
+    return Bool::make(self->isFlat());
+}
+
+QMETHOD(QGroupBox, setCheckable, Bool checkable)->void
+{
+    self->setCheckable(checkable);
+}
+
+QMETHOD(QGroupBox, setChecked, Bool checked)->void
+{
+    self->setChecked(checked);
+}
+
+QMETHOD(QGroupBox, setFlat, Bool flat)->void
+{
+    self->setFlat(flat);
+}
+
+QMETHOD(QGroupBox, setTitle, String title)->void
+{
+    self->setTitle(str::mbt_to_qt(title));
+}
+
+QMETHOD(QGroupBox, title)->String
+{
+    return str::qt_to_mbt(self->title());
+}
+
+SIGNAL_DEF(QGroupBox, toggled, Bool, Bool::make)
+
+COVARIANT(QGroupBox, QWidget)

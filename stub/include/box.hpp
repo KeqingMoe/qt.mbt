1c0bf0d51637ae4cb95bcfa8590f51dc7f46e6bd
Teru Shigure <me@keqing.moe>
2026-03-18 09:06:53 +0800

    feat: impl moonbit_trait for qt<T>

--- stub/include/box.hpp
+++ stub/include/box.hpp
@@ -51,6 +51,23 @@ struct qt
     }
 };
 
+template <typename T>
+struct mbt::moonbit_trait<qt<T>> : mbt::moonbit_trait<typename qt<T>::repr_type>
+{
+    using Self = qt<T>;
+    using Base = mbt::moonbit_trait<typename qt<T>::repr_type>;
+
+    static constexpr auto retain(Self self) noexcept -> void
+    {
+        Base::retain(self.repr);
+    }
+
+    static constexpr auto release(Self self) noexcept -> void
+    {
+        Base::release(self.repr);
+    }
+};
+
 #define COVARIANT(Derived, Base)                                                                                       \
     extern "C" auto Derived##_as_##Base(qt<Derived> self) -> qt<Base>                                                  \
     {                                                                                                                  \

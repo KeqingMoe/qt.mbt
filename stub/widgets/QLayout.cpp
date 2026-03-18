64d38fde6ce8e74995d2bcb611e5289ff311dc23
Teru Shigure <me@keqing.moe>
2026-03-18 13:08:50 +0800

    feat: improve QLayout

--- stub/widgets/QLayout.cpp
+++ stub/widgets/QLayout.cpp
@@ -7,4 +7,64 @@ QMETHOD(QLayout, addWidget, qt<QWidget> widget)->void
     self->addWidget(widget.get());
 }
 
+QMETHOD(QLayout, count)->Int
+{
+    return self->count();
+}
+
+QMETHOD(QLayout, indexOf, qt<QWidget> widget)->Int
+{
+    return self->indexOf(widget.get());
+}
+
+QMETHOD(QLayout, isEnabled)->Bool
+{
+    return Bool::make(self->isEnabled());
+}
+
+QMETHOD(QLayout, removeWidget, qt<QWidget> widget)->void
+{
+    self->removeWidget(widget.get());
+}
+
+QMETHOD(QLayout, setContentsMargins, Int left, Int top, Int right, Int bottom)->void
+{
+    self->setContentsMargins(left, top, right, bottom);
+}
+
+QMETHOD(QLayout, setEnabled, Bool value)->void
+{
+    self->setEnabled(value);
+}
+
+QMETHOD(QLayout, setSizeConstraint, Int constraint)->void
+{
+    self->setSizeConstraint(static_cast<QLayout::SizeConstraint>(constraint));
+}
+
+QMETHOD(QLayout, setSpacing, Int spacing)->void
+{
+    self->setSpacing(spacing);
+}
+
+QMETHOD(QLayout, sizeConstraint)->Int
+{
+    return self->sizeConstraint();
+}
+
+QMETHOD(QLayout, spacing)->Int
+{
+    return self->spacing();
+}
+
+QMETHOD(QLayout, unsetContentsMargins)->void
+{
+    self->unsetContentsMargins();
+}
+
+QMETHOD(QLayout, update)->void
+{
+    self->update();
+}
+
 COVARIANT(QLayout, QObject)

8af807178ea067f6df1a6157a32284652df0371f
Teru Shigure <me@keqing.moe>
2026-03-21 11:29:42 +0800

    build: replace pkg header with manual fetch

--- stub/xmake.lua
+++ stub/xmake.lua
@@ -1,17 +1,31 @@
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate", {outputdir = "."})
 
-add_repositories("local-repo vendor")
-add_requires("moonbridge")
-
 target("qt_mbt_stub")
     set_languages("c++23")
     add_rules("qt.static")
-    add_packages("moonbridge")
+    add_sysincludedirs("~/.moon/include")
     add_includedirs("include")
     add_files("**/*.cpp")
     add_frameworks("QtGui", "QtWidgets")
     
     after_build(function (target)
         print("XMAKE_TARGETDIR=%s", path.absolute(target:targetdir()))
+    end)
+    
+    on_load(function (target)
+        local incdir = path.join(target:autogendir(), "include")
+        target:add("includedirs", incdir)
+    end)
+
+    before_build(function (target)
+        import("net.http")
+
+        local incdir = path.join(target:autogendir(), "include")
+        local hdr = path.join(incdir, "moonbridge.hpp")
+        local url = "https://raw.githubusercontent.com/KeqingMoe/moonbridge/refs/heads/main/include/moonbridge.hpp"
+
+        os.mkdir(incdir)
+
+        http.download(url, hdr)
     end)

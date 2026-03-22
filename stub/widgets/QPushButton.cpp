b88e5d8a95b8b7d3cdc6153f4c63eaedba54fdf1
Teru Shigure <me@keqing.moe>
2026-03-22 14:45:53 +0800

    feat: add QPushButton::setFlat

--- stub/widgets/QPushButton.cpp
+++ stub/widgets/QPushButton.cpp
@@ -6,4 +6,14 @@ QSTATIC(QPushButton, new)->qt<QPushButton>
     return qt<QPushButton>::make();
 }
 
+QMETHOD(QPushButton, isFlat)->Bool
+{
+    return Bool::make(self->isFlat());
+}
+
+QMETHOD(QPushButton, setFlat, Bool flat)->void
+{
+    self->setFlat(flat);
+}
+
 COVARIANT(QPushButton, QAbstractButton)
